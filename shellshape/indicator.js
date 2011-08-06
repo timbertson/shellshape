4ed1fefc1f2e913cfa854231d47cbde099146f99
gfxmonk <tim@gfxmonk.net>
2011-08-06 11:37:43 +1000

    added icons to incidator

--- shellshape/indicator.js
+++ shellshape/indicator.js
@@ -9,10 +9,36 @@ function ShellshapeIndicator() {
 	this._init.apply(this, arguments);
 }
 
+
+
+function PopupImageMenuItem() {
+	this._init.apply(this, arguments);
+};
+
+PopupImageMenuItem.prototype = {
+	__proto__: PopupMenu.PopupBaseMenuItem.prototype,
+
+	_init: function (text, iconName, params) {
+		PopupMenu.PopupBaseMenuItem.prototype._init.call(this, params);
+
+		this.label = new St.Label({ text: text });
+		this._icon = new St.Icon({
+			icon_type: (St.IconType.FULLCOLOR), // TODO: shouldn't be necessary
+			style_class: 'popup-menu-icon'
+		});
+		this.addActor(this._icon, { align: St.Align.START });
+		this.addActor(this.label);
+		this.setIcon(iconName);
+	},
+
+	setIcon: function(name) {
+		this._icon.icon_name = name;
+	}
+};
+
 ShellshapeIndicator.prototype = {
 	__proto__: PanelMenu.SystemStatusButton.prototype,
 	_init: function() {
-		// TODO: 'folder'?
 		this.log = Log.getLogger("shellshape.indicator");
 		PanelMenu.SystemStatusButton.prototype._init.call(this, 'folder', 'Shellshape Layout');
 
@@ -21,34 +47,54 @@ ShellshapeIndicator.prototype = {
 			{
 				label: 'Floating',
 				action: this._untile_all,
+				icon: 'window-tile-floating-symbolic'
 				// activeText: 'X'
 			},
 			{
-				label: 'Tiled',
+				label: 'Horizontal',
 				action: this._tile_all,
+				icon: 'window-tile-horizontal-symbolic'
 				// activeText: 'Tiled'
 			}
+			// ,{
+			// 	label: 'Vertical',
+			// 	action: this._tile_all,
+			// 	icon: 'window-tile-vertical-symbolic'
+			// }
 		];
 		this.menu_indexes = {
 			floating: 0,
-			vertical: 1
+			horizontal: 1
 		};
 
 		var items = new PopupMenu.PopupMenuSection();
 		for(i in this.menu_entries) {
 			let item_props = this.menu_entries[i];
-			let item = new PopupMenu.PopupMenuItem(item_props.label);
+			let item = new PopupImageMenuItem(item_props.label, item_props.icon);
 			items.addMenuItem(item);
 			item.connect('activate', Lang.bind(this, function() {
 				this.log.debug("callback for [" + item_props.label + "] received by " + this);
-				this._set_text(item_props.label);
+				this._set_active_item(item_props);
 				item_props.action.call(this);
 			}));
 		}
 		this.menu.addMenuItem(items);
 
-		this.status_label = new St.Label({ text: this.menu_entries[0].label });
-		this.actor.set_child(this.status_label);
+		var default_entry = this.menu_entries[0];
+		this.box = new St.BoxLayout({});
+		this.icon = new St.Icon({
+			icon_type: (St.IconType.FULLCOLOR), // TODO: use proper symbolic icons
+			icon_name: default_entry.icon,
+			style_class: 'system-status-icon'
+		});
+		this.status_label = new St.Label({
+			text: default_entry.label,
+			style: "padding-left: 0.5em; min-width:5.5em;" // TODO: externalize style?
+		});
+		this.box.add_actor(this.icon);
+		this.box.add_actor(this.status_label);
+		this.actor.set_child(this.box);
+
 		this.meta_workspace = global.screen.get_workspace_by_index(global.screen.get_active_workspace_index());
 		this._update_indicator()
 
@@ -60,8 +106,9 @@ ShellshapeIndicator.prototype = {
 		return "<ShellshapeIndicator>";
 	},
 
-	_set_text: function(text) {
-		this.status_label.set_text(text);
+	_set_active_item: function(item) {
+		this.status_label.set_text(item.label);
+		this.icon.set_icon_name(item.icon);
 	},
 
 	_workspaceChanged: function(meta_screen, old_index, new_index) {
@@ -73,11 +120,11 @@ ShellshapeIndicator.prototype = {
 		//TODO: extend this when we have multiple tiling layouts
 		var item_props = null;
 		if(this.ext.get_workspace(this.meta_workspace).auto_tile) {
-			item_props = this.menu_entries[this.menu_indexes.vertical];
+			item_props = this.menu_entries[this.menu_indexes.horizontal];
 		} else {
 			item_props = this.menu_entries[this.menu_indexes.floating];
 		}
-		this._set_text(item_props.label);
+		this._set_active_item(item_props);
 	},
 
 	_tile_all: function() {
