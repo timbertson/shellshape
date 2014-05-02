83084fbed96b74d3cf3ea959608cefe794720e49
Tim Cuthbertson <tim@gfxmonk.net>
2014-05-02 21:29:22 +1000

    fix error from cleaning up workspace (by @tvn87 in #138)

--- shellshape/workspace.js
+++ shellshape/workspace.js
@@ -350,7 +350,7 @@ Workspace.prototype = {
 	on_window_remove: _duck_turbulence(_duck_overview(function(workspace, meta_window) {
 		let win = this.extension.get_window(meta_window);
 		this.log.debug("on_window_remove for " + win + " (" + this +")");
-		this.disconnect_workspace_signals(meta_window);
+		this.disconnect_workspace_signals(win);
 		this.layout.on_window_killed(win);
 		this.extension.remove_window(win);
 	})),
