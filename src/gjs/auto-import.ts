fb02ee9c7a98e33aac2138d4198a970623e8251a
Tim Cuthbertson <tim@gfxmonk.net>
2017-11-25 12:05:49 +1100

    improve autoimport

--- src/gjs/auto-import.ts
+++ src/gjs/auto-import.ts
@@ -25,47 +25,68 @@ function importAbsolute(path) {
 
 function makeImporter(basePath, scope, moduleName) {
 	let initial = importAbsolute(basePath + "/" + moduleName);
+	let impl = function() {
+		// first time round, just return `initial`. But replace `impl` so that next time
+		// we'll do a full reimport
+		impl = function() {
+			let canonicalPath = initial.__file__;
+			let tempPath = GLib.get_tmp_dir()
+			let temp = Gio.File.new_for_path(tempPath).get_child('gjs-autoimport');
+			if(!temp.query_exists(null)) {
+				myLog("creating " + temp.get_path())
+				temp.make_directory(null);
+			}
+			let genName = (function() {
+				let i = 0;
+				return function(base) {
+					i++;
+					return base + '@' + scope + '-' + i;
+				}
+			})();
 
-	let canonicalPath = initial.__file__;
-	let tempPath = GLib.get_tmp_dir()
-	let temp = Gio.File.new_for_path(tempPath).get_child('gjs-autoimport');
-	if(!temp.query_exists(null)) {
-		myLog("creating " + temp.get_path())
-		temp.make_directory(null);
-	}
-	let genName = (function() {
-		let i = 0;
-		return function(base) {
-			i++;
-			return base + '@' + scope + '-' + i;
-		}
-	})();
-
+			// only do initial setup once
+			impl = function() {
+				let candidate = genName(moduleName);
+				let candidateFilename = candidate + ".js";
+				// myLog("Checking candidate file: " + candidate);
+				let candidateFile = temp.get_child(candidateFilename);
+				if (candidateFile.query_exists(null)) {
+					GLib.unlink(candidateFile.get_path());
+					// myLog("Removing existing: " + candidate);
+				}
+				candidateFile.make_symbolic_link(canonicalPath, null);
+				return importAbsolute(temp.get_child(candidate).get_path());
+			}
+			return impl();
+		};
+		return initial;
+	};
 	return function() {
-		let candidate = genName(moduleName);
-		let candidateFilename = candidate + ".js";
-		// myLog("Checking candidate file: " + candidate);
-		let candidateFile = temp.get_child(candidateFilename);
-		if (candidateFile.query_exists(null)) {
-			GLib.unlink(candidateFile.get_path());
-			// myLog("Removing existing: " + candidate);
-		}
-		candidateFile.make_symbolic_link(canonicalPath, null);
-		return importAbsolute(temp.get_child(candidate).get_path());
-	}
+		return impl();
+	};
 }
 
-function wrapExtension(getExtension) {
+function wrapExtension(getExtension, scope) {
 	if(!ENABLED) {
 		myLog("Note: set GJS_AUTOIMPORT to \"!\" to enable reimports.");
 		return getExtension().init();
 	}
 
 	var currentInstance = null;
+	var notify = function() {
+		notify = function() {
+			try {
+				imports.ui.main.notify('Extension ' + scope + ' reloaded from disk');
+			} catch(e) {
+				myLog("notify failed");
+			}
+		}
+	}
 	return {
 		enable: function() {
 			currentInstance = getExtension().init();
 			currentInstance.enable();
+			notify();
 		},
 		disable: function() {
 			if (currentInstance !== null) {
@@ -81,5 +102,5 @@ function wrapExtensionModule(extension, name) {
 	let basePath = extension.path;
 	let scope = extension.uuid;
 	let getExtension = makeImporter(basePath, scope, name);
-	return wrapExtension(getExtension);
+	return wrapExtension(getExtension, scope);
 }
